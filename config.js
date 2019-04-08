cd9f2dbc370c0af131bd9023494b38d25202b316
Nick Pettazzoni <pettazz@gmail.com>
2019-04-08 12:54:47 -0400

    pull out config stuff, organize a little, add turndown config to keep tags with attrs

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	mysql: {
+	  host: "127.0.0.1",
+	  user: "root",
+	  password: "",
+	  database: "WNV"
+	},
+
+	markdown: {
+	  keepTags: ['iframe', 'script', 'style'],
+	  preserveAttrs: ['class', 'style']
+	},
+
+	paths: {
+	  postsOutDir: './posts'
+	}
+};
--- importer.js
+++ importer.js
@@ -1,20 +1,28 @@
 #!/usr/bin/env node
 
+// import
+const config = require('./config');
 const mysql = require('mysql'),
       fs = require('fs'),
       td = require('turndown'),
       slugify = require('slugify');
 
+// config
 const turndownService = new td({ codeBlockStyle: 'fenced' })
-                        .keep(['iframe', 'script', 'style']);
+        .keep(config.markdown.keepTags)
+        .addRule('preserveAttrs', {
+          filter: (node) => {
+            let attributes = config.markdown.preserveAttrs,
+                attrTest = attributes.some(attr => node.hasAttribute(attr)),
+                dataTest = Object.keys(node.dataset).length > 0;
 
-const conn = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "WNV"
-});
+            return attrTest || dataTest;
+          },
+          replacement: (innerHTML, node) => node.outerHTML})
 
+const conn = mysql.createConnection(config.mysql);
+
+// helper
 const generateFrontMatter = (blag) => {
   dateStr = toFullDateString(new Date(blag.posted * 1000));
   return `---
@@ -35,6 +43,38 @@ image:
 `;
 }
 
+const toDateString = (dateObj) => {
+  return dateObj.getFullYear() + '-' +
+         ('0' + (dateObj.getMonth() + 1)).slice(-2) + '-' +
+         ('0' + dateObj.getDate()).slice(-2);
+}
+
+const toFullDateString = (dateObj) => {
+  return toDateString(dateObj) + ' ' +
+         ('0' + (dateObj.getHours() + 1)).slice(-2) + ':' +
+         ('0' + (dateObj.getMinutes() + 1)).slice(-2) + ':' +
+         ('0' + (dateObj.getSeconds() + 1)).slice(-2) + ' ' +
+         // we are making some real assumptions about the timezone
+         '-0' + (dateObj.getTimezoneOffset() / 60) + '00';
+}
+
+
+// doing the work
+const createPostFile = (blag) => {
+  const time = new Date(blag.posted * 1000),
+        sluggedTitle = slugify(blag.title, {lower: true, remove: /[*+~.,#?/()'"!:@]/g}),
+        fileName = `${config.paths.postsOutDir}/${toDateString(time)}-${sluggedTitle}-${blag.guid}.md`;
+
+  let post = generateFrontMatter(blag) + turndownService.turndown(blag.content);
+
+  fs.writeFile(fileName, post, () => {
+    console.info(`Wrote ${fileName}`);
+  });
+}
+
+
+// main, i guess? what's that in node?
+// if i wanted to to this "right" i'd export a method but whatever
 conn.connect(function(err) {
   if (err) throw err;
   console.info("Connected");
@@ -51,30 +91,3 @@ conn.connect(function(err) {
     }
   });
 });
-
-const toDateString = (dateObj) => {
-  return dateObj.getFullYear() + '-' +
-         ('0' + (dateObj.getMonth() + 1)).slice(-2) + '-' +
-         ('0' + dateObj.getDate()).slice(-2);
-}
-
-const toFullDateString = (dateObj) => {
-  return toDateString(dateObj) + ' ' +
-         ('0' + (dateObj.getHours() + 1)).slice(-2) + ':' +
-         ('0' + (dateObj.getMinutes() + 1)).slice(-2) + ':' +
-         ('0' + (dateObj.getSeconds() + 1)).slice(-2) + ' ' +
-         // we are making some real assumptions about the timezone
-         '-0' + (dateObj.getTimezoneOffset() / 60) + '00';
-}
-
-const createPostFile = (blag) => {
-  const time = new Date(blag.posted * 1000),
-        sluggedTitle = slugify(blag.title, {lower: true, remove: /[*+~.,#?/()'"!:@]/g}),
-        fileName = `./posts/${toDateString(time)}-${sluggedTitle}-${blag.guid}.md`;
-
-  let post = generateFrontMatter(blag) + turndownService.turndown(blag.content);
-
-  fs.writeFile(fileName, post, () => {
-    console.log(`Wrote ${fileName}`);
-  });
-}
